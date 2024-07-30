055b86659dc6e463a702df60aa59c739c46d47df
mike-french <michaeltfrench@netscape.net>
2024-07-30 21:30:29 +0700

    Roll version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exa.Csv.MixProject do
     [
       app: :exa_csv,
       name: "Exa Csv",
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.15",
       erlc_options: [:verbose, :report_errors, :report_warnings, :export_all],
       start_permanent: Mix.env() == :prod,
